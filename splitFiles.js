7acd7e03b3f46225f540f3abf5e3322dc1b3736c
chiehan <chieh.an.hsu@gmail.com>
2017-03-24 17:44:23 +0800

    refactor splitFiles.js

--- splitFiles.js
+++ splitFiles.js
@@ -1,6 +1,6 @@
 var dbName = process.argv[2];
 var noBampoTag = process.argv[3];
-var inputPattern = './unSplittedTexts/**/*.*(txt|xml)';
+var globPattern = './' + dbName + '/' + dbName + '*/*.*(txt|xml)';
 var outputFolder = './splittedFolders';
 
 // modules
@@ -9,25 +9,25 @@ var fs = require('fs');
 var glob = require('glob');
 var mkdirp = require('mkdirp');
 var naturalSort = require('javascript-natural-sort');
-var split2BamposInVols = require('./index.js');
+var makeTextsInVols = require('./index.js');
 
 // input strings by the help of glob module and output files to ./splittedFolders
 
-var fileRoutes = glob.sync(inputPattern).sort(naturalSort);
+var originalTexts = glob.sync(inputPattern)
+  .sort(naturalSort);
+  .map(function(route) {
+    return fs.readFileSync(route, 'utf8')
+      .replace(/[\r\n]+/g, '\n')
+      .replace(/^\s*\n/gm, '');
+    });
 
-var texts = fileRoutes.map(function(route) {
-  return fs.readFileSync(route, 'utf8')
-    .replace(/[\r\n]+/g, '\n')
-    .replace(/^\s*\n/gm, '');
-});
+var textsInVols = makeTextsInVols(originalTexts, noBampoTag);
 
-var bamposInVols = split2BamposInVols(fileRoutes, texts, noBampoTag);
-
-bamposInVols.forEach(function(bamposInVol) {
-  var folder = './splittedFolders/' + dbName + bamposInVol.volN;
+textsInVols.forEach(function(textsInVol) {
+  var folder = './splittedFolders/' + dbName + textsInVol.volN;
   mkdirp.sync(folder);
-  bamposInVol.bampoObjs.forEach(function(bampoObj) {
-    var fileName = dbName + bampoObj.bampoN + '.xml';
-    fs.writeFileSync(folder + '/' + fileName, bampoObj.bampoText.trim(), 'utf8');
+  textsInVol.textObjs.forEach(function(textObj) {
+    var fileName = dbName + textObj.textN + '.xml';
+    fs.writeFileSync(folder + '/' + fileName, textObj.text.trim(), 'utf8');
   });
 });
